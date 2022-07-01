ee5b7c0389d42bb44b6659450181eb7dde18ec87
bobbi setiawan <bobbisetiawan@gmail.com>
2022-07-01 16:13:21 +0700

    Last session : order

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost/cwb_react-shopping-cart");
 
-// , {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// }
-
 const Product = mongoose.model("products", new mongoose.Schema({
     _id: {type: String, default: shortid.generate},
     title: String,
@@ -37,7 +31,35 @@ app.post("/api/products", async (req, res) => {
 app.delete("/api/products/:id", async (req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id);
     res.send(deletedProduct);
-})
+});
+
+const Order = mongoose.model('order', mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [{
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number
+    }]
+}, {
+    timestamps: true
+}));
+
+app.post('/api/orders', async(req, res) => {
+    if(!req.body.name || !req.body.email || !req.body.address || !req.body.cartItems || !req.body.total){
+        return res.send({message: 'Data is required'});
+    }
+
+    const order = await Order(req.body).save();
+    res.send(order);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
