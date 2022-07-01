7174199436c1956e732621761954d73da5b699f5
bobbi setiawan <bobbisetiawan@gmail.com>
2022-07-01 11:35:24 +0700

    Cart-redux

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-// feature - 1 test again
-
 import React from 'react';
-import data from "./data.json";
 import Products from "./components/Products";
+import Filter from './components/Filter';
+import Cart from './components/Cart';
+import store from './store';
+import {Provider} from 'react-redux';
 
-class App extends React.Component {
-
-  constructor(){
-    super();
-    this.state = {
-      products: data.products,
-      size: "",
-      sort: ""
-    }
-  }
-
+class App extends React.Component {  
   render(){
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/"> React Shopping Cart </a>
-        </header>
-        
-        <main>
-          <div className='content'>
-            <div className='main'> <Products products = {this.state.products}></Products> </div>
-            <div className='side-bar'> Cart Items </div>
-          </div>
-        </main>
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/"> React Shopping Cart </a>
+          </header>
+          
+          <main>
+            <div className='content'>
+              <div className='main'>
+                <Filter/>
+                <Products/>
+              </div>
+              <div className='side-bar'> 
+                <Cart/>
+              </div>
+            </div>
+          </main>
 
-        <footer>
-          All right is reserved.
-        </footer>
-      </div>
+          <footer>
+            All right is reserved.
+          </footer>
+        </div>
+      </Provider>
     );
   }
 }
